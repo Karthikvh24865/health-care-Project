e69cdcbe7387c68f23ae73c74b1c06ad5f9b8be2
Prabhu L K <133681882+prabhulk25@users.noreply.github.com>
2025-03-28 17:59:58 +0530

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,31 @@
+resource "aws_instance" "test-server" {
+  ami           = "ami-00bb6a80f01f03502" 
+  instance_type = "t2.micro" 
+  key_name = "Prabhu"
+  vpc_security_group_ids= ["sg-0ce7a707b375e270d"]
+  connection {
+    type     = "ssh"
+    user     = "ubuntu"
+    private_key = file("./Prabhu.pem")
+    host     = self.public_ip
+  }
+  provisioner "remote-exec" {
+    inline = [ "echo 'wait to start instance' "]
+  }
+  tags = {
+    Name = "Kubernetes-server"
+  }
+ // provisioner "local-exec" {
+//      command = " echo ${aws_instance.test-server.public_ip} > /etc/ansible/hosts "
+//  }
+// provisioner "local-exec" {
+//  command = "echo ${aws_instance.test-server.public_ip} | sudo tee /etc/ansible/hosts > /dev/null "
+//}
+  provisioner "local-exec" {
+    command = "echo '${self.public_ip} ansible_ssh_user=ubuntu ansible_ssh_private_key_file=./Prabhu.pem' | sudo tee -a /etc/ansible/hosts > /dev/null"
+  }
+
+   provisioner "local-exec" {
+  command = "ansible-playbook /var/lib/jenkins/workspace/Banking-finance-project/my-serverfiles/ansible-playbook.yml "
+  } 
+}
