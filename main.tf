b3a38a763fb54d61cb7740b18de8e1f69fbf7cbe
Karthikvh24865 <111963116+Karthikvh24865@users.noreply.github.com>
2025-05-04 07:49:49 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -6,7 +6,7 @@ resource "aws_instance" "Kubernetes-server" {
   connection {
     type     = "ssh"
     user     = "ubuntu"
-    private_key = file("./Prabhu.pem")
+    private_key = file("./huli.pem")
     host     = self.public_ip
   }
  /* provisioner "remote-exec" {
@@ -26,7 +26,7 @@ resource "aws_instance" "Kubernetes-server" {
 //  command = "echo ${aws_instance.Kubernetes-server.public_ip} | sudo tee /etc/ansible/hosts > /dev/null "
 //}
  // provisioner "local-exec" {
-  //  command = "echo '${self.public_ip} ansible_ssh_user=ubuntu ansible_ssh_private_key_file=./Prabhu.pem' | sudo tee -a /etc/ansible/hosts > /dev/null"
+  //  command = "echo '${self.public_ip} ansible_ssh_user=ubuntu ansible_ssh_private_key_file=./huli.pem' | sudo tee -a /etc/ansible/hosts > /dev/null"
 //  }
 
  //  provisioner "local-exec" {
